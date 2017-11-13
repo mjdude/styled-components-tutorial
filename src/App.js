ffcdfc735b8416151ae21165014146713dd1598e
mjdude <mo@syncspot.net>
2017-11-12 18:31:05 -0800

    added insian red

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import styled from "styled-components";
+
+const mainColor = `indianred`;
+
+const Title = styled.h1`
+ color: ${props => props.color || 'goldenrod'}
+`;
 
 class App extends Component {
   render() {
     return (
       <div>
-        Mystagram
+        <Title color={mainColor}>Mystagram</Title>
       </div>
     );
   }
