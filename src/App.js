c395a69135a723f5dd974b290ead911682c07894
mjdude <mo@syncspot.net>
2017-11-12 18:41:57 -0800

    updated class style

--- src/App.js
+++ src/App.js
@@ -5,17 +5,27 @@ const mainColor = `indianred`;
 
 const Title = styled.h1`
  color: ${props => props.color || 'goldenrod'}
+ font-size: 2.8em;
+ margin: 25px;
+ padding-bottom: 20px;
+ border-bottom: 20px;
+ border-bottom: 1px solid ${mainColor};
+ display: inlineblock;
 `;
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className={this.props.className}>
         <Title color={mainColor}>Mystagram</Title>
       </div>
     );
   }
 }
 
-export default App;
+export default styled(App)`
+  width: 100vw;
+  height: 100vh;
+  text-align: center;
+`;
 
